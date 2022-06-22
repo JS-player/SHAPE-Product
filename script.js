54c89aa784c8d9b75e7cf1688c5a0f2e8a0e349a
Mahmoud Abdelrahman <47402937+JS-player@users.noreply.github.com>
2022-06-22 09:31:11 +0200

    fixing problems - 1

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-var step = 300;
+
+
+var step = 100;
 		var scrolling = true;
 
-		// Wire up events for the 'scrollUp' link:
-		$(".preSlide").bind("click", function(event) {
+
+			$(".preSlide").bind("click", function(event) {
 			event.preventDefault();
-			// Animates the scrollTop property by the specified
-			// step.
+
 			$("div").animate({
 				scrollLeft: "-=" + step + "px"
 			});
 		});
 
-		$(".nextSlide").bind("click", function(event) {
+
+
+			$(".nextSlide").bind("click", function(event) {
 			event.preventDefault();
 			$("div").animate({
 				scrollLeft: "+=" + step + "px"
 			});
 		})
+
+
+
+
+
+
